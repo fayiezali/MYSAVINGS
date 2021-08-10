32788948a794ef20057ccc3be2b0d311b7c8cb89
Fayiez Ali <fayiez@outlook.com>
2021-08-10 13:47:57 +0300

    Add Pages HTML

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+# تسجيل التطبيقات التي أنشأتها في المشروع
+    'crispy_forms', # 01 جميلة - التطبيق رقم  "Login.html"  هذا التطبيق وظيفته يجعل شكل صفحة
+    'commons', # التطبيق رقم 02
+    # 'accounts', # التطبيق رقم 03
+    # 'social_django',# 04 Login With Social Media(Facebook , Instagram ,......)
+    # 'django_countries',# تطبيق معد مسبقاً يحتوي على جميع اسماء دول العالم
 ]
 
 MIDDLEWARE = [
@@ -116,10 +124,55 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [ BASE_DIR / "static"]# تحديد مسار الملف وهو الموقع الذي سوف يحبث  في المشروع عن هذا الملف
+# '/media/':  الوسائط المتعددة"Media" بإدارة ملفات الـ "static" يقوم مجلد 
+# '/media/': التي يحتاجها المشروع "Documents ,Images , Audio , Video,"هو مجلد  يتحوي على جميع ملفات 
+# '/media/': يتم إستدعائها عن طريق هذا المجلد . هذاالمجلد فيه جميع إعدادت وتنسيقات الصفحة  "app"في التطبيق  "html"أي صفحة 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (BASE_DIR /'media')#تحديد مسار الملف وهو الموقع الذي سوف يحبث  في المشروع عن هذا الملف
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#
+LOGIN_REDIRECT_URL = 'Index_URL' # Go To Home Page HTML
+LOGOUT_REDIRECT_URL = 'My_LogoutDone_URL' # Go To LogoutDone.html Page HTML
+LOGIN_ERROR_URL = '/login/'
+
+#Call Class Sign  In with Email
+AUTHENTICATION_BACKENDS = ['accounts.backends.EmailBackend']
+
+# EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+
+# Email Settings
+# from accounts.email_info import EMAIL_BACKEND , EMAIL_HOST , EMAIL_HOST_USER , EMAIL_HOST_PASSWORD , EMAIL_PORT ,  EMAIL_USE_TLS , PASSWORD_RESET_TIMEOUT_DAYS
+# EMAIL_BACKEND = EMAIL_BACKEND
+# EMAIL_HOST = EMAIL_HOST # mail service smtp
+# EMAIL_HOST_USER = EMAIL_HOST_USER# email id
+# EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD # password
+# EMAIL_PORT = EMAIL_PORT
+# EMAIL_USE_TLS = EMAIL_USE_TLS
+# PASSWORD_RESET_TIMEOUT_DAYS = PASSWORD_RESET_TIMEOUT_DAYS
+#
+#
+# social auth
+# SOCIAL_AUTH_URL_NAMESPACE = 'social'
+# SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
+# SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
--- PROJECT/urls.py
+++ PROJECT/urls.py
@@ -14,8 +14,36 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path ,include
+from django.conf import settings
+from django.conf.urls.static import static
+#
+#
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+#
+#
+urlpatterns += [
+    path('commons/', include('commons.urls')),# Path App Public_Pages    
+]
+#
+#
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')), # This Path Coming From Django App
+]
+#
+#
+#
+#
+#
+#
+# Download These Folders If There are No Errors In the Serveer
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
+#
+#
+# urlpatterns += [
+#     path('', include('social_django.urls')),
+# ]
