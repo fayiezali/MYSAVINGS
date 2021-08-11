d44c7452b0d140e86a7f560fda0786b66bfed87a
Fayiez Ali <fayiez@outlook.com>
2021-08-12 01:08:27 +0300

    Add Pages Base & Index/Home

--- commons/urls.py
+++ commons/urls.py
@@ -4,7 +4,7 @@ from commons import views
 #
 #
 urlpatterns = [
-    path(''      , views.Index.as_view(), name='Index_URL'),
-    path('about/'    , views.About.as_view(), name='About_URL'),
+    path(''        , views.Index.as_view(), name='Index_URL'),
+    path('about/'  , views.About.as_view(), name='About_URL'),
     
 ] 
