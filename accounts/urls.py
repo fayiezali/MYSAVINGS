da3bc4ba70bd5b0037fb94952bdcc7cb43940a9b
Fayiez Ali <fayiez@outlook.com>
2021-08-14 11:45:01 +0300

    Add Password Change

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,21 +6,24 @@ from accounts import views
 # 
 #
 #
-# URL For Authentication (1)
+# URL For Authentication (1) Login
 urlpatterns = [
         # Login In System   
-        path('accounts/my_login/'                            , views.My_Login.as_view()                  , name='My_Login_URL'),
+        path('accounts/my_login/'                  , views.My_Login.as_view()                  , name='My_Login_URL'),
         # Exit From System
-        path('accounts/my_logout/'                           , views.My_Logout.as_view()                 , name='My_Logout_URL'),
+        path('accounts/my_logout/'                 , views.My_Logout.as_view()                 , name='My_Logout_URL'),
         # Checkout Confirmed Successfull
-        path('accounts/my_logout_done/'                      , views.My_LogoutDone.as_view()             , name='My_LogoutDone_URL'),
-        #*********************************************************************************
-        # # Change Password
-        # path('my_password_change/'                 , views.My_PasswordChange.as_view()         , name='My_PasswordChange_URL'),
-        # # Password Change completed Succesfull
-        # path('my_password_change_done/'            , views.My_PasswordChangeDone.as_view()     , name='My_PasswordChangeDone_URL'),
+        path('accounts/my_logout_done/'            , views.My_LogoutDone.as_view()             , name='My_LogoutDone_URL'),
 ]
 #
 #
 # URL For Authentication (2)
+urlpatterns += [
+        # Change Password
+        path('my_password_change/'                 , views.My_PasswordChange.as_view()         , name='My_PasswordChange_URL'),
+        # Password Change completed Succesfull
+        path('my_password_change_done/'            , views.My_PasswordChangeDone.as_view()     , name='My_PasswordChangeDone_URL'),
+]
+#
+#
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User # إستيراد اسم المستخدم
-from django.contrib.auth.views import LoginView , LogoutView ,TemplateView
+from django.contrib.auth.views import ( LoginView , LogoutView ,
+                                        TemplateView , PasswordChangeView
+)
+from django.urls import reverse_lazy
+
 #
 #
 #
@@ -22,6 +26,20 @@ class My_Logout(LogoutView):
 # Display Them About Page
 class My_LogoutDone(TemplateView):
     template_name = 'registration/my_logout_done.html' # The Page HTML to Display
+#
+#
+#
+# Confirm Checkout From System
+class My_PasswordChange(PasswordChangeView):
+    template_name = 'registration/my_password_change.html' # The Page HTML to Display
+    success_url = reverse_lazy('My_PasswordChangeDone_URL') # Go to This Page After Successful Operation
+#
+#
+#
+#Confirm Password Change
+class My_PasswordChangeDone(TemplateView):
+    template_name = 'registration/my_password_change_done.html'# The Page HTML to Display
+    # title = ('password change successful')
 #
 #
 #
